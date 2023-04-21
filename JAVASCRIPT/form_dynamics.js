95cfc1256e78858552737217e08a5a86aeb26b3f
unknown <gabriel.santos1518@etec.sp.gov.br>
2023-04-21 17:50:09 -0300

    3th_commit-minor_desing_changes_and_adjustments

--- JAVASCRIPT/form_dynamics.js
+++ JAVASCRIPT/form_dynamics.js
@@ -3,15 +3,15 @@
 
 function showHideInput(sel) {
   var value = sel.value;  
-  if(value==0)
+  if(value==1)
       document.getElementById('dvd').style.display = 'block';
   else
       document.getElementById('dvd').style.display = 'none';
-  if(value==1)
+  if(value==2)
       document.getElementById('furniture').style.display = 'block';
   else
       document.getElementById('furniture').style.display = 'none';
-  if(value==2)
+  if(value==3)
       document.getElementById('book').style.display = 'block';
   else
       document.getElementById('book').style.display = 'none';
